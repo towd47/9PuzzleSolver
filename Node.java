a7247aa54506763cf14fe05a778f57533e234e66
Tom Pescatore <tpescatore11@gmail.com>
2016-01-03 15:29:53 -0500

    removed duplicate code in node and puzzle, puzzle now uses node instead of having its own state

--- Node.java
+++ Node.java
@@ -6,21 +6,53 @@
 public class Node {
 	int[][] state;
 	Node previousNode;
+	private int blankRow;
+	private int blankCol;
 
+	public Node(int size) {
+		int currentVal = 0;
+		state = new int[size][size];
+		for (int row = 0; row < size; row++) {
+			for (int col = 0; col < size; col++) {
+				state[row][col] = currentVal;
+				currentVal++;
+			}
+		}
+		blankRow = 0;
+		blankCol = 0;
+	}
+	
 	//Creates a new node with the given state
 	public Node(int[][] state) {
 		this.state = state;
 		previousNode = null;
+		for (int row = 0; row < state.length; row++) {
+			for (int col = 0; col < state[0].length; col++) {
+				if (state[row][col] == 0) {
+					blankRow = row;
+					blankCol = col;
+					break;
+				}
+			}
+		}
 	}
 	
 	//Creates a new node with the given state and sets a pointer to the node this one was reached from
 	public Node(int[][] state, Node previousNode) {
 		this.state = state;
 		this.previousNode = previousNode;
+		for (int row = 0; row < state.length; row++) {
+			for (int col = 0; col < state[0].length; col++) {
+				if (state[row][col] == 0) {
+					blankRow = row;
+					blankCol = col;
+					break;
+				}
+			}
+		}
 	}
 	
-	//returns the state of the node in string form
-	public String getState() {
+	public String toString() {
 		String currentState = "";
 		for (int i = 0; i < state.length; i++) {
 			for (int j = 0; j < state[0].length; j++) {
@@ -43,5 +75,49 @@ public class Node {
 			}
 		}
 		return stateCopy;
+	}
+	
+	//Swaps the zero representing the blank with the space to the left or returns false if the zero has no spaces to the left
+	public boolean moveBlankLeft() {
+		if (blankCol != 0) {
+			state[blankRow][blankCol] = state[blankRow][blankCol - 1];
+			state[blankRow][blankCol - 1] = 0;
+			blankCol--;
+			return true;
+		}
+		return false;
+	}
+
+	//Swaps the zero representing the blank with the space to the right or returns false if the zero has no spaces to the right
+	public boolean moveBlankRight() {
+		if (blankCol != state.length - 1) {
+			state[blankRow][blankCol] = state[blankRow][blankCol + 1];
+			state[blankRow][blankCol + 1] = 0;
+			blankCol++;
+			return true;
+		}
+		return false;
+	}
+
+	//Swaps the zero representing the blank with the space above or returns false if the zero has no spaces above it
+	public boolean moveBlankUp() {
+		if (blankRow != 0) {
+			state[blankRow][blankCol] = state[blankRow - 1][blankCol];
+			state[blankRow - 1][blankCol] = 0;
+			blankRow--;
+			return true;
+		}
+		return false;
+	}
+
+	//Swaps the zero representing the blank with the space below or returns false if the zero has no spaces below it
+	public boolean moveBlankDown() {
+		if (blankRow != state[0].length - 1) {
+			state[blankRow][blankCol] = state[blankRow + 1][blankCol];
+			state[blankRow + 1][blankCol] = 0;
+			blankRow++;
+			return true;
+		}
+		return false;
 	}
 }
--- Puzzle.java
+++ Puzzle.java
@@ -5,51 +5,48 @@
 */
 
 public class Puzzle {
-	int[][] puzzleState;
-	String goalStateStr;
-	int blankRow;
-	int blankCol;
-	int lastMove = -1;
-	int movesRandomized;
-
+	Node node;
+	private int[][] goalState;
+	
 	//Constructor
 	public Puzzle(int size) {
-		puzzleState = new int[size][size];
+		node = new Node(size);
+		goalState = getGoalStateArr();
+	}
+	public Puzzle(Node node) {
+		this.node = node;
+		goalState = getGoalStateArr();
 	}
 
-	//Initializes the puzzle to the goal state
-	public void initializePuzzle() {
-		int currentVal = 0;
-		for (int row = 0; row < puzzleState.length; row++) {
-			for (int col = 0; col < puzzleState[0].length; col++) {
-				puzzleState[row][col] = currentVal;
-				currentVal++;
-			}
+	public static void showSolution(Node node) {
+
+		// Creates solution string and prints solution to console
+		String solution = "";
+		int movesToSolve = 0;
+
+		while (node.previousNode != null) {
+			solution = node.toString() + "\n" + solution;
+			node = node.previousNode;
+			movesToSolve++;
 		}
-		blankRow = 0;
-		blankCol = 0;
-		movesRandomized = 0;
-		this.goalStateStr = getGoalState();
+		solution = node.toString() + "\n" + solution;
+		System.out.print(solution);
+		System.out.println("Took " + movesToSolve + " moves to solve puzzle.");
 	}
 	
-	//Initializes the puzzle to the state input
-	public void initializePuzzle(int[][] state) {
-		puzzleState = state;
-		for (int row = 0; row < puzzleState.length; row++) {
-			for (int col = 0; col < puzzleState[0].length; col++) {
-				if (puzzleState[row][col] == 0) {
-					blankRow = row;
-					blankCol = col;
-					break;
+	public boolean isSolved() {
+		for (int row = 0; row < node.state.length; row++) {
+			for (int col = 0; col < node.state[0].length; col++) {
+				if(node.state[row][col] != goalState[row][col]) {
+					return false;
 				}
 			}
 		}
-		movesRandomized = -1;
-		this.goalStateStr = getGoalState();
+		return true;
 	}
-
-	//Randomizes the puzzle with a given number of moves and prints out the randomized state
+	
 	public void randomizePuzzle(int moves) {
+		int lastMove = -1;
 		int movesDone = 0;
 		boolean moved;
 		while (movesDone < moves) {
@@ -58,117 +55,38 @@ public class Puzzle {
 				direction = (int) (Math.random() * 4);
 			}
 			if (direction == 0) {
-				moved = moveBlankLeft();
+				moved = node.moveBlankLeft();
 				if (moved) lastMove = 1;
 			}
 			else if (direction == 1) {
-				moved = moveBlankRight();
+				moved = node.moveBlankRight();
 				if (moved) lastMove = 0;
 			}
 			else if (direction == 2) {
-				moved = moveBlankUp();
+				moved = node.moveBlankUp();
 				if (moved) lastMove = 3;
 			}
 			else {
-				moved = moveBlankDown();
+				moved = node.moveBlankDown();
 				if (moved) lastMove = 2;
 			}
 			if (moved) {
 				movesDone++;
 			}
-			//getState(true);
+			//System.out.println(node.toString);
 			//TO SEE EACH STEP OF THE RANDOMIZER FOR TESTING
 			//UNCOMMENT THE LINE ABOVE
 		}
-		movesRandomized = movesDone;
 		System.out.println("Randomized state is:");
-		getState(true);
-	}
-
-	//Swaps the zero representing the blank with the space to the left or returns false if the zero has no spaces to the left
-	public boolean moveBlankLeft() {
-		if (blankCol != 0) {
-			puzzleState[blankRow][blankCol] = puzzleState[blankRow][blankCol - 1];
-			puzzleState[blankRow][blankCol - 1] = 0;
-			blankCol--;
-			return true;
-		}
-		return false;
-	}
-
-	//Swaps the zero representing the blank with the space to the right or returns false if the zero has no spaces to the right
-	public boolean moveBlankRight() {
-		if (blankCol != puzzleState.length - 1) {
-			puzzleState[blankRow][blankCol] = puzzleState[blankRow][blankCol + 1];
-			puzzleState[blankRow][blankCol + 1] = 0;
-			blankCol++;
-			return true;
-		}
-		return false;
-	}
-
-	//Swaps the zero representing the blank with the space above or returns false if the zero has no spaces above it
-	public boolean moveBlankUp() {
-		if (blankRow != 0) {
-			puzzleState[blankRow][blankCol] = puzzleState[blankRow - 1][blankCol];
-			puzzleState[blankRow - 1][blankCol] = 0;
-			blankRow--;
-			return true;
-		}
-		return false;
-	}
-
-	//Swaps the zero representing the blank with the space below or returns false if the zero has no spaces below it
-	public boolean moveBlankDown() {
-		if (blankRow != puzzleState[0].length - 1) {
-			puzzleState[blankRow][blankCol] = puzzleState[blankRow + 1][blankCol];
-			puzzleState[blankRow + 1][blankCol] = 0;
-			blankRow++;
-			return true;
-		}
-		return false;
-	}
-
-	//Returns a copy of the current state of the puzzle, used to create nodes without them pointing to the puzzle state variable
-	public int[][] getStateArr() {
-		int[][] arrCopy = new int[puzzleState.length][puzzleState[0].length];
-		for (int i = 0; i < puzzleState.length; i++) {
-			for (int j = 0; j < puzzleState.length; j++) {
-				arrCopy[i][j] = puzzleState[i][j];
-			}
-		}
-		return arrCopy;
+		System.out.println(node.toString());
 	}
 	
-	//Returns the current state of the puzzle in string form. Also prints the puzzle if the input argument is true
-	public String getState(boolean print) {
-		String currentState = "";
-		for (int i = 0; i < puzzleState.length; i++) {
-			for (int j = 0; j < puzzleState.length; j++) {
-				currentState = currentState + puzzleState[i][j];
-				if (j != puzzleState.length - 1) {
-					currentState = currentState + " ";
-				}
+	private int[][] getGoalStateArr() {
+		int[][] goalState = new int[node.state.length][node.state[0].length];
+		for (int i = 0; i < node.state.length; i++) {
+			for (int j = 0; j < node.state.length; j++) {
+				goalState[i][j] = (i*node.state.length) + j;
 			}
-			currentState = currentState + "\n";
-		}
-		if (print) {
-			System.out.println(currentState);
-		}
-		return currentState;
-	}
-	
-	//Used to create a string to represent the goalstate of the puzzle when the puzzle is initialized
-	private String getGoalState() {
-		String goalState = "";
-		for (int i = 0; i < puzzleState.length; i++) {
-			for (int j = 0; j < puzzleState.length; j++) {
-				goalState = goalState + ((i*puzzleState.length) + j);
-				if (j != puzzleState.length - 1) {
-					goalState = goalState + " ";
-				}
-			}
-			goalState = goalState + "\n";
 		}
 		return goalState;
 	}
